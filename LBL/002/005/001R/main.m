b4aa236f3d904e77882ee668894e5d30637ec571
RobertPolicelli <75266261+RobertPolicelli@users.noreply.github.com>
2024-10-28 16:51:21 -0400

    Add files via upload

---
+++ LBL/002/005/001R/main.m
@@ -0,0 +1,25 @@
+Experiment.StartNewSection('Experiment Assets');
+
+% load features values
+oLBL = ExperimentManager.Load('LBL-002-005-001');
+oLabelledFeatureValues = oLBL.GetLabelledFeatureValues();
+
+
+% Remove 2 BM that did not have RANO measurements
+vbKeepSample = true(oLabelledFeatureValues.GetNumberOfSamples(),1);
+vbKeepSample(108) = false;
+vbKeepSample(24) = false;
+oLabelledFeatureValues = oLabelledFeatureValues(vbKeepSample,:);
+
+% Create new feature values object
+oRecord = CustomFeatureExtractionRecord("LBL-002-005-001R", "In-field Progression", oLabelledFeatureValues.GetFeatures());
+
+oLabelledFeatureValues = LabelledFeatureValuesByValue(...
+    oLabelledFeatureValues.GetFeatures(), oLabelledFeatureValues.GetGroupIds(), oLabelledFeatureValues.GetSubGroupIds(), oLabelledFeatureValues.GetUserDefinedSampleStrings(), oLabelledFeatureValues.GetFeatureNames(),...
+    oLabelledFeatureValues.GetLabels(), oLabelledFeatureValues.GetPositiveLabel(), oLabelledFeatureValues.GetNegativeLabel(),...
+    'FeatureExtractionRecord', oRecord);
+
+oLBL = Labels("LBL-002-005-001R");
+
+oLBL.SaveLabelledFeatureValuesAsMat(oLabelledFeatureValues);
+oLBL.Save();
