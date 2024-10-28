13f18429a344c9a8979dbea240e15f99be2f3475
RobertPolicelli <75266261+RobertPolicelli@users.noreply.github.com>
2024-10-28 16:48:56 -0400

    Add files via upload

---
+++ FV/004/002/005R/main.m
@@ -0,0 +1,24 @@
+Experiment.StartNewSection('Experiment Assets');
+
+% load features values
+oFV = ExperimentManager.Load('FV-004-002-005A');
+oFeatureValues = oFV.GetFeatureValues();
+
+% Remove 2 BM that did not have RANO measurements
+vbKeepSample = true(oFeatureValues.GetNumberOfSamples(),1);
+vbKeepSample(108) = false;
+vbKeepSample(24) = false;
+oFeatureValues = oFeatureValues(vbKeepSample,:);
+
+% Create new feature values object
+oRecord = CustomFeatureExtractionRecord("FV-004-002-005R", oFeatureValues.GetFeatureExtractionRecord(1).GetFeatureExtractionRecordPortions.GetDescription(), oFeatureValues.GetFeatures());
+
+oFeatureValues = FeatureValuesByValue(...
+    oFeatureValues.GetFeatures(), oFeatureValues.GetGroupIds(), oFeatureValues.GetSubGroupIds(), oFeatureValues.GetUserDefinedSampleStrings(), oFeatureValues.GetFeatureNames(),...
+    'FeatureIsCategorical', oFeatureValues.IsFeatureCategorical(),...
+    'FeatureExtractionRecord', oRecord);
+
+oFV = ExperimentFeatureValues("FV-004-002-005R");
+
+oFV.SaveFeatureValuesAsMat(oFeatureValues);
+oFV.Save();
